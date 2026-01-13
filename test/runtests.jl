4a2635f3c12d85ce8ccc5fd92524936e4cb5e0a4
Kevin Bonham <dev@bonham.ch>
2026-01-13 15:30:32 -0500

    Add Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,26 @@ module BioJuliaTemplateTest
 using BioJuliaTemplate
 
 using Test
+using Aqua
 
 @testset "Approximation" begin
     x = MyType(3, 4)
     @test isapprox(my_function(x), 5)
+end
+
+@testset "Aqua" begin
+    Aqua.test_all(BioJuliaTemplate; 
+        # # Override defaults:
+        # ambiguities = false,
+        # unbound_args = false,
+        # undefined_exports = false,
+        # project_extras = false,
+        # stale_deps = false,
+        # deps_compat = false,
+        # piracies = false,
+        # persistent_tasks = false,
+        # undocumented_names = true,
+    )
 end
 
 end # module BioJuliaTemplateTest
